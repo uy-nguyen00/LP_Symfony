ca855645e3976bdbe7393be2b9c6906ac7648974
Uy N <cao.nguyen@etu.umontpellier.fr>
2023-03-04 14:13:54 +0100

    update BandFixtures.php

--- src/DataFixtures/BandFixtures.php
+++ src/DataFixtures/BandFixtures.php
@@ -12,10 +12,11 @@ class BandFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $band1 = new Band();
-        $band1->setBandName('band1')
+        $band1->setBandName('Kinjaz')
             ->addArtistMember($this->getReference(ArtistFixtures::ARTIST_SEAN_LEW))
             ->addArtistMember($this->getReference(ArtistFixtures::ARTIST_ANTHONY_LEE))
-            ->addArtistMember($this->getReference(ArtistFixtures::ARTIST_BAM_MARTIN));
+            ->addArtistMember($this->getReference(ArtistFixtures::ARTIST_BAM_MARTIN))
+            ->setPicture('kinjaz.jpeg');
         $manager->persist($band1);
 
         $manager->flush();
