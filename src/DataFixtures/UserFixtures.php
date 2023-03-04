84de0427ff9b1e12da55b64a31b2ca310172b8d1
Uy N <cao.nguyen@etu.umontpellier.fr>
2023-03-04 14:02:39 +0100

    update UserFixtures.php: add role_admin

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,26 +6,37 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture implements DependentFixtureInterface
 {
+    private UserPasswordHasherInterface $hasher;
+
     public const USER_ADMIN = 'user-admin';
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
 
     public function load(ObjectManager $manager): void
     {
-        $u1 = new User();
-        $u1->setPseudo('admin')
-            ->setRole('admin')
-            ->setUserName('The Admin')
-            ->addFavoriteArtist($this->getReference(ArtistFixtures::ARTIST_SEAN_LEW));
-        $manager->persist($u1);
 
+        $u1 = new User();
+        $u1->setUsername('admin')
+            ->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($u1, 'admin');
+        $u1->setPassword($password);
+//            ->addFavoriteArtist($this->getReference(ArtistFixtures::ARTIST_SEAN_LEW));
+
+        $manager->persist($u1);
         $manager->flush();
 
         $this->setReference(self::USER_ADMIN, $u1);
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
             ArtistFixtures::class
