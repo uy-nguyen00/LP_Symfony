0bd7dd2bf7dfc129fbb4c29c6a04edb15ec6772e
Uy N <cao.nguyen@etu.umontpellier.fr>
2023-03-06 20:21:12 +0100

    artist CRUD

---
+++ src/Controller/ArtistController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Artist;
+use App\Form\ArtistType;
+use App\Repository\ArtistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/artist')]
+class ArtistController extends AbstractController
+{
+    #[Route('/', name: 'app_artist_index', methods: ['GET'])]
+    public function index(ArtistRepository $artistRepository): Response
+    {
+        return $this->render('artist/index.html.twig', [
+            'artists' => $artistRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_artist_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ArtistRepository $artistRepository): Response
+    {
+        $artist = new Artist();
+        $form = $this->createForm(ArtistType::class, $artist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($pictureName = $this->savePictureFile($form)) {
+                $artist->setPicture($pictureName);
+            }
+
+            $artist->setPicture($pictureName);
+            $artistRepository->save($artist, true);
+
+            return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('artist/new.html.twig', [
+            'artist' => $artist,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_artist_show', methods: ['GET'])]
+    public function show(Artist $artist): Response
+    {
+        return $this->render('artist/show.html.twig', [
+            'artist' => $artist,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_artist_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Artist $artist, ArtistRepository $artistRepository): Response
+    {
+        $form = $this->createForm(ArtistType::class, $artist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($pictureName = $this->savePictureFile($form)) {
+                $artist->setPicture($pictureName);
+            }
+            $artistRepository->save($artist, true);
+
+            $this->addFlash('success', 'Artist updated successfully!');
+            return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('artist/edit.html.twig', [
+            'artist' => $artist,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_artist_delete', methods: ['POST'])]
+    public function delete(Request $request, Artist $artist, ArtistRepository $artistRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $artist->getId(), $request->request->get('_token'))) {
+            $artistRepository->remove($artist, true);
+        }
+
+        return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function savePictureFile(FormInterface $form): string|bool
+    {
+        /** @var UploadedFile $uploadedFile */
+        $uploadedFile = $form['pictureFile']->getData();
+
+        if ($uploadedFile) {
+            $destinationFolder = $this->getParameter('kernel.project_dir') . '/public/images/artists';
+            $pictureName = uniqid() . '.' . $uploadedFile->guessExtension();
+            $uploadedFile->move($destinationFolder, $pictureName);
+
+            return $pictureName;
+        }
+        return false;
+    }
+}
